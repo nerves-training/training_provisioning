fd09379a35de39cbdb8b60b0d42d2d0cd0fbda47
Frank Hunleth <fhunleth@troodon-software.com>
2019-10-15 21:00:41 -0400

    Minor doc and template updates

--- mix.exs
+++ mix.exs
@@ -15,6 +15,7 @@ defmodule Starter.MixProject do
       build_embedded: true,
       aliases: [loadconfig: [&bootstrap/1]],
       deps: deps(),
+      dialyzer: dialyzer(),
       releases: [{@app, release()}],
       preferred_cli_target: [run: :host, test: :host]
     ]
@@ -43,6 +44,7 @@ defmodule Starter.MixProject do
       {:shoehorn, "~> 0.6"},
       {:ring_logger, "~> 0.6"},
       {:toolshed, "~> 0.2"},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
 
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.6", targets: @all_targets},
@@ -73,6 +75,12 @@ defmodule Starter.MixProject do
       include_erts: &Nerves.Release.erts/0,
       steps: [&Nerves.Release.init/1, :assemble],
       strip_beams: Mix.env() == :prod
+    ]
+  end
+
+  defp dialyzer() do
+    [
+      flags: [:race_conditions, :unmatched_returns, :error_handling, :underspecs]
     ]
   end
 end
